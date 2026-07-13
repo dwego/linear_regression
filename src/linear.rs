39bdcd5abaa851736007f6265407e1f0e60998a7
dweg0 <dweg0epic@outlook.com>
2026-07-12 23:50:44 -0300

    feat: add gradient descent training for linear regression

--- src/linear.rs
+++ src/linear.rs
@@ -1,10 +1,15 @@
 pub struct LinearRegression {
-    weight: f64,
-    bias: f64
+    pub weight: f64,
+    pub bias: f64,
 }
 
 impl LinearRegression {
-    pub fn fit_mse (x: &[f64], y: &[f64]) -> Result<Self, &'static str>  {
+    pub fn fit_mse(
+        x: &[f64],
+        y: &[f64],
+        alpha: f64,
+        epochs: usize,
+    ) -> Result<Self, &'static str> {
         if x.len() != y.len() {
             return Err("x e y precisam ter o mesmo tamanho");
         }
@@ -13,26 +18,51 @@ impl LinearRegression {
             return Err("são necessários pelo menos dois pontos");
         }
 
-        let initial_weight = 0.0;
-        let initial_bias = 0.0;
+        if alpha <= 0.0 {
+            return Err("alpha precisa ser maior que zero");
+        }
 
-        let loss_func_derivate_weight = 2.0
-            * x.iter()
-                .zip(y)
-                .map(|(&x_i, &y_i)| (initial_weight * x_i + initial_bias - y_i) * x_i)
+        if epochs == 0 {
+            return Err("epochs precisa ser maior que zero");
+        }
+
+        let mut weight = 0.0;
+        let mut bias = 0.0;
+
+        let n = x.len() as f64;
+
+        for _ in 0..epochs {
+            let loss_derivative_weight = 2.0
+                * x.iter()
+                .zip(y.iter())
+                .map(|(&x_i, &y_i)| {
+                    let prediction = weight * x_i + bias;
+                    let error = prediction - y_i;
+
+                    error * x_i
+                })
                 .sum::<f64>()
-            / x.len() as f64;
+                / n;
 
-        let loss_func_derivate_bias = 2.0
-            * x.iter()
-                .zip(y)
-                .map(|(&x_i, &y_i)| initial_weight * x_i + initial_bias - y_i)
+            let loss_derivative_bias = 2.0
+                * x.iter()
+                .zip(y.iter())
+                .map(|(&x_i, &y_i)| {
+                    let prediction = weight * x_i + bias;
+
+                    prediction - y_i
+                })
                 .sum::<f64>()
-            / x.len() as f64;
+                / n;
 
-        Ok(Self {
-            weight: 2.0,
-            bias: 0.0
-        })
+            weight -= alpha * loss_derivative_weight;
+            bias -= alpha * loss_derivative_bias;
+        }
+
+        Ok(Self { weight, bias })
+    }
+
+    pub fn predict(&self, x: f64) -> f64 {
+        self.weight * x + self.bias
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use linear_regression::linear::LinearRegression;
 
 fn main() {
-    let lr = LinearRegression::fit(&[1.0, 2.0], &[2.0, 4.0]).unwrap();
+    let lr = LinearRegression::fit_mse(&[1.0, 2.0], &[2.0, 4.0], 0.01, 100).unwrap();
+
+    println!("Weight: {}", lr.weight);
+    println!("Bias: {}", lr.bias);
+
 }
